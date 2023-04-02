1cbec77fc852d381f457f5ba55754506f1b3601b
Nicholas <nicholasdev@zohomail.com>
2023-04-02 13:04:38 -0300

    Exercício finalizado

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿int numero;
+
+Console.WriteLine("Digite um número inteiro: ");
+numero = int.Parse(Console.ReadLine()!);
+int centenas = numero / 100;
+int dezenas = (numero / 10) % 10;
+int unidades = numero % 10;
+
+Console.WriteLine($"O número {numero} possui: ");
+Console.WriteLine($"{dezenas} Dezena (s)");
+Console.WriteLine($"{unidades} Unidade (s)");
+Console.WriteLine($"{centenas} Centena (s)");
