76d7dd4ccd0bf9a65c3ba97d598b3667dd17aa33
Nicholas <nicholasdev@zohomail.com>
2023-04-02 13:11:58 -0300

    Exercício finalizado

--- Program.cs
+++ Program.cs
@@ -1,11 +1,18 @@
-﻿int numero;
+﻿//Declaração de variável
+int numero;
 
+Console.WriteLine("Decomposição Decimal");
+
+//Pede para o usuário digitar um número inteiro
 Console.WriteLine("Digite um número inteiro: ");
 numero = int.Parse(Console.ReadLine()!);
+
+//Realiza as operações para descobrir as unidades, dezenas e centenas
 int centenas = numero / 100;
-int dezenas = (numero / 10) % 10;
+int dezenas = (numero / 100) % 10;
 int unidades = numero % 10;
 
+//Exibe as dezenas, unidades, centenas
 Console.WriteLine($"O número {numero} possui: ");
 Console.WriteLine($"{dezenas} Dezena (s)");
 Console.WriteLine($"{unidades} Unidade (s)");
